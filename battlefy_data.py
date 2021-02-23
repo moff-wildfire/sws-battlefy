2f3137fdca23feba3abfee07c8d614ae668604e0
moff-wildfire <70867805+moff-wildfire@users.noreply.github.com>
2021-02-23 01:14:04 -0600

    Add DL of team logos

--- battlefy_data.py
+++ battlefy_data.py
@@ -1,7 +1,28 @@
 import urllib.request
+import urllib.parse
 import json
+import os
+import unicodedata
+import re
 
 tournament_api = 'https://dtmwra1jsgyb0.cloudfront.net/'
+
+
+def slugify(value, allow_unicode=False):
+    """
+    Taken from https://github.com/django/django/blob/master/django/utils/text.py
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-\s]+', '-', value).strip('-_')
 
 
 class BattlefyData(object):
@@ -75,6 +96,14 @@ class BattlefyData(object):
                                                        'stage-' + str(stage_number+1) + '_round-'
                                                        + str(match['roundNumber']) + '_match-'
                                                        + str(match['matchNumber']) + '_' + game + '.png')
+
+    def dl_team_logos(self):
+        for team in self.tournament_data['teams']:
+            url = self.tournament_data['teams'][team]['persistentTeam']['logoUrl']
+            if url:
+                extension = os.path.splitext(urllib.parse.urlparse(url)[2])[1]
+                team_name = slugify(self.tournament_data['teams'][team]['name'])
+                urllib.request.urlretrieve(url, team_name + extension)
 
 
 def main():
